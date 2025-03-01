75f0b9abf269cc540c2d34e49b954944a73c7e0b
Kinetic Dev <kinetic.fivemdev@gmail.com>
2025-02-28 21:47:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -149,11 +149,6 @@ function printNote(title, description, date) {
           setTimeout(() => window.close(), 100);
         };
       </script>
-      <footer style="text-align: center; padding: 20px;">
-        <p>Kinetic Dev Notes App</p>
-        <p>&copy; 2025 Kinetic Dev. All rights reserved.</p>
-      </footer>
-
     </body>
     </html>
   `);
